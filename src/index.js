4584fffb592288e83662e845a10d255e3139b2d6
domkli <dominika.w.klim@gmail.com>
2021-07-20 17:30:03 +0200

    updates added

---
+++ src/index.js
@@ -0,0 +1,72 @@
+let currentTime = document.querySelector("#current-date");
+let now = new Date();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let day = now.getDay();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+currentTime.innerHTML = `${days[day]} ${hours}:${minutes}`;
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let cityElement = document.querySelector("#name");
+
+  console.log(response.data);
+  let celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  cityElement.innerHTML = response.data.name;
+}
+
+function search(city) {
+  let apiKey = "c22d7cfa34c632413da8b22d306863b3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  window.axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function searchLocation(position) {
+  let apiKey = "c22d7cfa34c632413da8b22d306863b3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}& appid=${apiKey}&units=metric`;
+  window.axios.get(apiUrl).then(displayTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+search("Barcelona");
